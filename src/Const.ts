4d1bf0bd3ae93ca8c916548c34f03ce62d01d14c
xuyunshi <405029644@qq.com>
2022-10-14 17:35:53 +0800

    Update checkbox const

--- src/Const.ts
+++ src/Const.ts
@@ -1,6 +1,6 @@
-export const defaultSelKeys = ["createdat", "message", "os", "suid", "uuid", "platform", "nativeversion", "sdkversion"];
+export const defaultSelKeys = ["createdat", "message", "os", "suid", "level", "platform", "nativeversion", "sdkversion"];
 export const optionKeys = [
     { title: '常用信息', list: ["createdat", "message", "level", "platform", "os"] },
-    { title: '追踪信息', list: ["clientip", "region", "suid", "uuid", "session", "team", "app", "vid"] },
+    { title: '追踪信息', list: ["clientip", "region", "suid", "userid", "uuid", "session", "team", "app"] },
     { title: '版本信息', list: ["akkoversion", "nativeversion", "sdkversion", "netlessua"] }
 ];
