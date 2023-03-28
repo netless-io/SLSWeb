e552616bd92772fa435597df9b75cc926e2d21e5
xuyunshi <405029644@qq.com>
2023-03-28 18:28:07 +0800

    Update title, icon, description

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -4,8 +4,12 @@ import {
   RouterProvider
 } from 'react-router-dom'
 import router from './Router';
+import { useTranslation } from 'react-i18next';
 
 export default function WrappedApp() {
+  const {t} = useTranslation();
+
+  document.title = t("web.title");
   return (
     <Suspense fallback="... is loading">
       <RouterProvider router={router} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useTransition } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Pages/App';
 import './index.css';
